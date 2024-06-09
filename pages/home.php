e868e75006fb2767ce7d390f145ad109a2b931f2
Luiz Serão <luiz.serao@hotmail.com>
2024-06-09 06:03:39 -0300

    features-home

--- pages/home.php
+++ pages/home.php
@@ -4,3 +4,25 @@
   <p>Most calendars are designed for teams. Slate is designed for freelancers</p>
   <a href="/">Try For Free</a>
 </section>
+
+<section class="features">
+  <h2 class="features-title">Features</h2>
+  <p class="features-description">Most calendars are designed for teams. Slate is designed for freelancers</p>
+  <ul class="features-list">
+    <li>
+      <img src="<?php echo INCLUDE_PATH; ?>/assets/mdi_drawing.svg" alt="">
+      <h3>OpenType features Variables fonts</h3>
+      <p>Slate helps you see how many more days you need to work to reach your financial goal.</p>
+    </li>
+    <li>
+      <img src="<?php echo INCLUDE_PATH; ?>/assets/mdi_draw.svg" alt="">
+      <h3>Design with real data</h3>
+      <p>Slate helps you see how many more days you need to work to reach your financial goal.</p>
+    </li>
+    <li>
+      <img src="<?php echo INCLUDE_PATH; ?>/assets/mdi_brush.svg" alt="">
+      <h3>Fastest way to take action</h3>
+      <p>Slate helps you see how many more days you need to work to reach your financial goal.</p>
+    </li>
+  </ul>
+</section>
