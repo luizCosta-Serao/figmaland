8861a758b816b1119aecbfe6dee31e19177cb999
Luiz Serão <luiz.serao@hotmail.com>
2024-06-13 05:57:08 -0300

    loggout do painel de controle e mais HTML/CSS

--- painel/main.php
+++ painel/main.php
@@ -1,1 +1,29 @@
-<h1>Bem-Vindo <?php echo $_SESSION['user'] ?> ;D</h1>
+<?php
+  // Loggout do Painel de Controle
+  if (isset($_GET['loggout'])) {
+    Painel::loggout();
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL; ?>/css/style.css">
+  <title>Painel de Controle</title>
+</head>
+<body>
+  <header class="header">
+    <div>
+      <a href="<?php echo INCLUDE_PATH_PAINEL; ?>/?loggout">Sair</a>
+    </div>
+    </header>
+    
+  <aside class="header-left">
+
+  </aside>
+  <section class="container">
+
+  </section>
+</body>
+</html>
