d52b8c45f9c34b60f0e878320a2358cd5f2db4c4
Luiz Serão <luiz.serao@hotmail.com>
2024-06-15 05:30:56 -0300

    interface inicial painel controle

--- painel/main.php
+++ painel/main.php
@@ -37,7 +37,7 @@
         // Se img estiver preenchida adicionar a mesma 
         } else { 
       ?>
-        <div class="img-usuario">
+        <div class="photo-usuario">
           <img src="<?php echo INCLUDE_PATH_PAINEL; ?>/uploads/<?php echo $_SESSION['img'] ?>" alt="">
         </div>
       <?php } ?>
@@ -47,10 +47,41 @@
         <p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
       </div>
     </div>
+    <nav class="menu-aside">
+      <ul>
+        <li class="category-menu">Cadastro</li>
+        <li><a href="">Cadastrar Feature</a></li>
+        <li><a href="">Cadastrar Testimonial</a></li>
+        <li><a href="">Cadastrar Pricing</a></li>
+        <li class="category-menu">Gestão</li>
+        <li><a href="">Listar Features</a></li>
+        <li><a href="">Listar Testimonials</a></li>
+        <li><a href="">Listar Pricings</a></li>
+        <li class="category-menu">Administração do Painel</li>
+        <li><a href="">Editar Usuário</a></li>
+        <li><a href="">Adicionar Usuários</a></li>
+        <li class="category-menu">Configuração Geral</li>
+        <li><a href="">Editar</a></li>
+      </ul>
+    </nav>
   </aside>
 
   <section class="container">
-    
+    <h1>Painel de Controle</h1>
+    <ul class="metrics">
+      <li class="metrics-single users-online">
+        <h1>Usuários Online</h1>
+        <p>10</p>
+      </li>
+      <li class="metrics-single visits-total">
+        <h1>Total de Visitas</h1>
+        <p>100</p>
+      </li>
+      <li class="metrics-single visits-today">
+        <h1>Visitas Hoje</h1>
+        <p>3</p>
+      </li>
+    </ul>
   </section>
 
   <script src="<?php echo INCLUDE_PATH; ?>/js/jquery-3.7.1.min.js"></script>
